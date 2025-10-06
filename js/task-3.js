212c83aad8b360d723532a5b637924f637f8ed67
Ella <ella.tseluiko@gmail.com>
2025-10-05 20:34:29 -0500

    Add js tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,11 @@
-
+function checkForSpam(message) {
+	const lowerCaseMessage = message.toLowerCase();
+	if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+		return true
+	} else {
+		return false;
+	}
+}
 
 
 
